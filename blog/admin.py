166c49ad7b1f95a3f7f57be64e3435f7df98f5bb
Anshit <anshitverma55@gmail.com>
2020-12-03 23:39:17 +0530

    Initial Commit

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from blog.models import Post, Comment
+# Register your models here.
+# SU
+# Username-anshit
+# Password-SuperUs3r
+
+
+admin.site.register(Post)
+admin.site.register(Comment)
